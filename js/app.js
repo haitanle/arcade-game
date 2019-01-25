290502e504bc42e909c592ec987c40364ae2a82b
tan <haitanle88@gmail.com>
2019-01-25 12:21:39 -0500

    player movement

--- js/app.js
+++ js/app.js
@@ -19,6 +19,7 @@ Enemy.prototype.update = function(dt) {
     // all computers.
    //console.log(window.ctx.canvas.width);
    if (this.x > window.ctx.canvas.width){
+    //reset back to original position 
     this.x = -150;
    }else{
     this.x = (this.x + this.speed*dt);
@@ -37,16 +38,32 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(){
+var Player = function(x,y){
 
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = 'images/char-boy.png';
+    this.x = x; 
+    this.y = y;
 }
 
 
-Player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+Player.prototype.update = function() {
+    //handle off screen? 
+
+    //function handleCanvasWEdge 
+    const canvasWEdge = window.ctx.canvas.width -105;
+    if (this.x > canvasWEdge){
+            this.x = canvasWEdge;
+    }else if (this.x < 0){
+            this.x = 0;
+    }
+    
+    //handle collison? 
+
+    //handle when goal is reached? 
+
+
+
+
 };
 
 // Draw the player on the screen, required method for game
@@ -55,8 +72,24 @@ Player.prototype.render = function() {
 };
 
 
-Player.prototype.handleInput = function(){
+Player.prototype.handleInput = function(key){
+    console.log(key);
 
+    const moveY = 80;
+    const moveX = 100;
+
+    switch(key){
+        case 'up': this.y-= moveY;
+                    break;
+        case 'down': this.y+= moveY;
+                    break;
+        case 'left': this.x-= moveX;
+                    break;
+        case 'right': this.x+= moveX;
+                    break;
+    }
+
+    console.log(`x position ${this.x}, y position ${this.y}`)
 }
 
 
@@ -66,7 +99,7 @@ Player.prototype.handleInput = function(){
 
 let allEnemies = [];
 
-let player = new Player();
+let player = new Player(100,300);
 
 let enemey1 = new Enemy(-150,145,50);
 let enemey2 = new Enemy(-150,225,150);
