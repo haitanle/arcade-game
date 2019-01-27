0977d1ddf33556d9c6f365f4e3ce10e2b371a4f7
tan <haitanle88@gmail.com>
2019-01-26 22:06:49 -0500

    game functions meet requirement

--- js/app.js
+++ js/app.js
@@ -1,14 +1,23 @@
+//pass an object instead of making it a global variable 
+
+window.playerPosX = 0;
+window.playerPosY = 0;
+window.isPlayerHit = false;
+
 // Enemies our player must avoid
-var Enemy = function(x,y,speed) {
+var Enemy = function(y,speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = x;
+    this.startX = -150;
+    this.x = this.startX; //intial position
     this.y = y;
     this.speed = speed;
+
+    this.size = {top: 15, bottom: 15, left: 70, right: 70}
 };
 
 // Update the enemy's position, required method for game
@@ -17,21 +26,32 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
    //console.log(window.ctx.canvas.width);
    if (this.x > window.ctx.canvas.width){
     //reset back to original position 
-    this.x = -150;
+    this.x = this.startX;
    }else{
     this.x = (this.x + this.speed*dt);
    }
    //this.y = (this.y + (50*dt));
 
-   // console.log(this.x);
+   //check if the enemy has hit the player
+   const enemyHeight = {topHeight: this.y - this.size.top, lowHeight: this.y + this.size.bottom};
+   const enemyWidth = {leftWidth: this.x - this.size.left, rightWidth: this.x + this.size.right};
+
+   if ((window.playerPosY >= enemyHeight.topHeight && window.playerPosY <= enemyHeight.lowHeight) && 
+            (window.playerPosX >= enemyWidth.leftWidth && window.playerPosX <= enemyWidth.rightWidth) 
+    ){
+        console.log('player hit!!!');
+        window.playerPosX = 0;
+        window.playerPosY = 0;
+        window.isPlayerHit = true;
+   }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    //console.log(this.sprite);
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -41,25 +61,52 @@ Enemy.prototype.render = function() {
 var Player = function(x,y){
 
     this.sprite = 'images/char-boy.png';
-    this.x = x; 
-    this.y = y;
+    this.startX = 200;
+    this.startY = 380;
+    this.x = this.startX; 
+    this.y = this.startY;
+
+    this.moveY = 80;
+    this.moveX = 100;
+
+    this.size = {top: 15, bottom: 15, left: 40, right: 40}
 }
 
 
 Player.prototype.update = function() {
+    //handle collison? 
+    if (isPlayerHit){
+
+        //rotate the player
+
+        this.x = this.startX;
+        this.y = this.startY;
+        window.isPlayerHit = false;
+    }
+
     //handle off screen? 
 
-    //function handleCanvasWEdge 
-    const canvasWEdge = window.ctx.canvas.width -105;
-    if (this.x > canvasWEdge){
-            this.x = canvasWEdge;
+    //function handle canvasEdgeWidth 
+    const  canvasEdgeWidth = window.ctx.canvas.width - (this.size.left + this.size.right);
+    if (this.x >  canvasEdgeWidth){
+            this.x -= this.moveX;
     }else if (this.x < 0){
             this.x = 0;
     }
-    
-    //handle collison? 
+
+    const canvasEdgeHeight = window.ctx.canvas.height - ((this.size.top + this.size.bottom)*5);
+    if (this.y > canvasEdgeHeight){
+        this.y -= this.moveY;
+    }
 
     //handle when goal is reached? 
+    const winLine = 60;
+
+    if (this.y < winLine){
+        this.x = this.startX;
+        this.y = this.startY;
+        console.log('Game won!');
+    }
 
 
 
@@ -68,26 +115,35 @@ Player.prototype.update = function() {
 
 // Draw the player on the screen, required method for game
 Player.prototype.render = function() {
+
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+    //TODO: rotate player when hit
+    //ctx.translate(Resources.get(this.sprite),20,20);
+    //ctx.rotate(Resources.get(this.sprite), 20 * Math.PI / 180);
+    //ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+//ctx.restore();
+
+    
 };
 
 
 Player.prototype.handleInput = function(key){
     console.log(key);
 
-    const moveY = 80;
-    const moveX = 100;
-
     switch(key){
-        case 'up': this.y-= moveY;
+        case 'up': this.y-= this.moveY;
                     break;
-        case 'down': this.y+= moveY;
+        case 'down': this.y+= this.moveY;
                     break;
-        case 'left': this.x-= moveX;
+        case 'left': this.x-= this.moveX;
                     break;
-        case 'right': this.x+= moveX;
+        case 'right': this.x+= this.moveX;
                     break;
     }
+
+    window.playerPosX = this.x;
+    window.playerPosY = this.y;
 
     console.log(`x position ${this.x}, y position ${this.y}`)
 }
@@ -99,11 +155,11 @@ Player.prototype.handleInput = function(key){
 
 let allEnemies = [];
 
-let player = new Player(100,300);
+let player = new Player();
 
-let enemey1 = new Enemy(-150,145,50);
-let enemey2 = new Enemy(-150,225,150);
-let enemey3 = new Enemy(-150,55,100);
+let enemey1 = new Enemy(145,50);
+let enemey2 = new Enemy(225,150);
+let enemey3 = new Enemy(55,100);
 
 
 allEnemies.push(enemey1);
