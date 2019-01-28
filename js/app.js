99403da75ac176a1a5ae18cf136b680524feb67f
tan <haitanle88@gmail.com>
2019-01-28 10:59:24 -0500

    code cleanup

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-//pass an object instead of making it a global variable 
 
 window.playerPosX = 0;
 window.playerPosY = 0;
@@ -6,18 +5,16 @@ window.isPlayerHit = false;
 
 // Enemies our player must avoid
 var Enemy = function(y,speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+
     this.sprite = 'images/enemy-bug.png';
+    this.size = {top: 15, bottom: 15, left: 70, right: 70}
+
     this.startX = -150;
-    this.x = this.startX; //intial position
+    this.x = this.startX;
     this.y = y;
     this.speed = speed;
-
-    this.size = {top: 15, bottom: 15, left: 70, right: 70}
 };
 
 // Update the enemy's position, required method for game
@@ -26,29 +23,32 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-
-   //console.log(window.ctx.canvas.width);
    if (this.x > window.ctx.canvas.width){
-    //reset back to original position 
-    this.x = this.startX;
+        this.x = this.startX;
    }else{
-    this.x = (this.x + this.speed*dt);
+        this.x = (this.x + this.speed*dt);
    }
-   //this.y = (this.y + (50*dt));
 
-   //check if the enemy has hit the player
-   const enemyHeight = {topHeight: this.y - this.size.top, lowHeight: this.y + this.size.bottom};
-   const enemyWidth = {leftWidth: this.x - this.size.left, rightWidth: this.x + this.size.right};
+   detectCollision(this);
+};
+
+/*
+* detectCollision function
+* input: enemy object
+* Using enemy location and size to determine if collision with player's position
+*/
+function detectCollision(enemy){
+    const enemyHeight = {topHeight: enemy.y - enemy.size.top, lowHeight: enemy.y + enemy.size.bottom};
+    const enemyWidth = {leftWidth: enemy.x - enemy.size.left, rightWidth: enemy.x + enemy.size.right};
 
    if ((window.playerPosY >= enemyHeight.topHeight && window.playerPosY <= enemyHeight.lowHeight) && 
             (window.playerPosX >= enemyWidth.leftWidth && window.playerPosX <= enemyWidth.rightWidth) 
     ){
-        console.log('player hit!!!');
         window.playerPosX = 0;
         window.playerPosY = 0;
         window.isPlayerHit = true;
    }
-};
+}
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -61,6 +61,8 @@ Enemy.prototype.render = function() {
 var Player = function(x,y){
 
     this.sprite = 'images/char-boy.png';
+    this.size = {top: 15, bottom: 15, left: 40, right: 40}
+
     this.startX = 200;
     this.startY = 380;
     this.x = this.startX; 
@@ -68,68 +70,61 @@ var Player = function(x,y){
 
     this.moveY = 80;
     this.moveX = 100;
-
-    this.size = {top: 15, bottom: 15, left: 40, right: 40}
 }
 
-
 Player.prototype.update = function() {
-    //handle collison? 
+
     if (isPlayerHit){
-
-        //rotate the player
-
         this.x = this.startX;
         this.y = this.startY;
         window.isPlayerHit = false;
     }
+    detectEdge(this); 
+    detectWin(this);
 
-    //handle off screen? 
+}
 
-    //function handle canvasEdgeWidth 
-    const  canvasEdgeWidth = window.ctx.canvas.width - (this.size.left + this.size.right);
-    if (this.x >  canvasEdgeWidth){
-            this.x -= this.moveX;
-    }else if (this.x < 0){
-            this.x = 0;
+/*
+* detectEdge function
+* adjust the player's object position using canvas edge
+* input: player object
+*/
+function detectEdge(player){
+    const  canvasEdgeWidth = window.ctx.canvas.width - (player.size.left + player.size.right);
+    if (player.x >  canvasEdgeWidth){
+            player.x -= player.moveX;
+    }else if (player.x < 0){
+            player.x = 0;
     }
 
-    const canvasEdgeHeight = window.ctx.canvas.height - ((this.size.top + this.size.bottom)*5);
-    if (this.y > canvasEdgeHeight){
-        this.y -= this.moveY;
-    }
+    const canvasEdgeHeight = window.ctx.canvas.height - ((player.size.top + player.size.bottom)*5);
+    if (player.y > canvasEdgeHeight){
+        player.y -= player.moveY;
+    }   
+}
 
-    //handle when goal is reached? 
+
+/*
+* detectWin function 
+* determine whether player has crossed finishing line
+* input- player object
+*/ 
+function detectWin(player){
     const winLine = 60;
 
-    if (this.y < winLine){
-        this.x = this.startX;
-        this.y = this.startY;
-        console.log('Game won!');
+    if (player.y < winLine){
+        player.x = player.startX;
+        player.y = player.startY;
     }
-
-
-
-
-};
+}
 
 // Draw the player on the screen, required method for game
 Player.prototype.render = function() {
-
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-
-    //TODO: rotate player when hit
-    //ctx.translate(Resources.get(this.sprite),20,20);
-    //ctx.rotate(Resources.get(this.sprite), 20 * Math.PI / 180);
-    //ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-//ctx.restore();
-
-    
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y); 
 };
 
 
 Player.prototype.handleInput = function(key){
-    console.log(key);
 
     switch(key){
         case 'up': this.y-= this.moveY;
@@ -144,8 +139,6 @@ Player.prototype.handleInput = function(key){
 
     window.playerPosX = this.x;
     window.playerPosY = this.y;
-
-    console.log(`x position ${this.x}, y position ${this.y}`)
 }
 
 
@@ -155,17 +148,17 @@ Player.prototype.handleInput = function(key){
 
 let allEnemies = [];
 
+let enemy1 = new Enemy(145,50);
+let enemy2 = new Enemy(225,150);
+let enemy3 = new Enemy(55,100);
+let enemy4 = new Enemy(145,200);
+
+allEnemies.push(enemy1);
+allEnemies.push(enemy2);
+allEnemies.push(enemy3);
+allEnemies.push(enemy4);
+
 let player = new Player();
-
-let enemey1 = new Enemy(145,50);
-let enemey2 = new Enemy(225,150);
-let enemey3 = new Enemy(55,100);
-
-
-allEnemies.push(enemey1);
-allEnemies.push(enemey2);
-allEnemies.push(enemey3);
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
